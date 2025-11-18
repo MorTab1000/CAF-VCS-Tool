f785fc6da9e88c7f38f3dafc9bc677eaa211497b
hilata01 <hilata01@gmail.com>
2025-11-18 15:34:51 +0200

    Added tag class header

---
+++ libcaf/src/tag.h
@@ -0,0 +1,15 @@
+#ifndef TAG_H
+#define TAG_H
+
+#include <string>
+
+class Tag {
+public:
+    const std::string commit_hash;
+    const std::string name;
+
+    Tag(const std::string& commit_hash, const std::string& name):
+    commit_hash(commit_hash), name(name) {}
+};
+
+#endif // TAG_H
