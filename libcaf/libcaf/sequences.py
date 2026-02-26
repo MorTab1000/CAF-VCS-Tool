a287228eccb026b5aa0730f2f54267c3b7dee2e9
MorTab1000 <morproject10@gmail.com>
2026-02-26 15:34:14 +0200

    Refactor LineSequence

--- libcaf/libcaf/sequences.py
+++ libcaf/libcaf/sequences.py
@@ -1,35 +1,34 @@
-from typing import IO, Sequence, Union, List, Tuple
+from typing import IO, Sequence, Union
 import mmap
-import os
 
 class LinesSequence(Sequence[bytes]):
-    def __init__(self, file_obj: IO[bytes]):
-        self._file = file_obj
-        self._line_offsets: List[Tuple[int, int]] = []
-        if os.fstat(self._file.fileno()).st_size == 0:
-            self._mm = b""  # Use empty bytes, not mmap
-            # self._line_offsets remains []
-            return
-        self._mm = mmap.mmap(self._file.fileno(), 0, access=mmap.ACCESS_READ)
-        self._build_line_offsets()
+    def __init__(self, mmap_obj: mmap.mmap):
+        # self._file = file_obj
+        self._line_offsets  = None
+        self._mm = mmap_obj
 
-    def _build_line_offsets(self) -> None:
-        start = 0
-        mm_len = len(self._mm)
+    def _ensure_offsets(self) -> None:
+        """Builds the index only if it hasn't been built yet."""
+        if self._line_offsets is None:
+            self._line_offsets = []
+            start = 0
+            mm_len = len(self._mm)
 
-        while start < mm_len:
-            end = self._mm.find(b"\n", start)
-            if end == -1:
-                # last line without '\n'
-                self._line_offsets.append((start, mm_len))
-                break
-            self._line_offsets.append((start, end + 1))
-            start = end + 1
+            while start < mm_len:
+                end = self._mm.find(b"\n", start)
+                if end == -1:
+                    # last line without '\n'
+                    self._line_offsets.append((start, mm_len))
+                    break
+                self._line_offsets.append((start, end + 1))
+                start = end + 1
 
     def __len__(self) -> int:
+        self._ensure_offsets()
         return len(self._line_offsets)
 
     def __getitem__(self, index: Union[int, slice]):
+        self._ensure_offsets()
         if isinstance(index, slice):
             return [self[i] for i in range(*index.indices(len(self)))]
 
@@ -39,12 +38,4 @@ class LinesSequence(Sequence[bytes]):
         start, end = self._line_offsets[index]
         return self._mm[start:end]
 
-    def close(self) -> None:
-        if isinstance(self._mm, mmap.mmap):
-            self._mm.close()
 
-    def __enter__(self) -> "LinesSequence":
-        return self
-
-    def __exit__(self, exc_type, exc, tb) -> None:
-        self.close()
