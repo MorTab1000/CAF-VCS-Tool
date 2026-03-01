10527c4ab4ead20984b6e00b6481b3305fa56cc7
MorTab1000 <morproject10@gmail.com>
2026-03-01 15:32:33 +0200

    Updating sequences.py

--- libcaf/libcaf/sequences.py
+++ libcaf/libcaf/sequences.py
@@ -1,34 +1,17 @@
 from typing import IO, Sequence, Union
 import mmap
+import os
+from contextlib import contextmanager
 
 class LinesSequence(Sequence[bytes]):
-    def __init__(self, mmap_obj: mmap.mmap):
-        # self._file = file_obj
-        self._line_offsets  = None
+    def __init__(self, mmap_obj: mmap.mmap, line_offsets: list[tuple[int, int]]):
+        self._line_offsets  = line_offsets
         self._mm = mmap_obj
 
-    def _ensure_offsets(self) -> None:
-        """Builds the index only if it hasn't been built yet."""
-        if self._line_offsets is None:
-            self._line_offsets = []
-            start = 0
-            mm_len = len(self._mm)
-
-            while start < mm_len:
-                end = self._mm.find(b"\n", start)
-                if end == -1:
-                    # last line without '\n'
-                    self._line_offsets.append((start, mm_len))
-                    break
-                self._line_offsets.append((start, end + 1))
-                start = end + 1
-
     def __len__(self) -> int:
-        self._ensure_offsets()
         return len(self._line_offsets)
 
     def __getitem__(self, index: Union[int, slice]):
-        self._ensure_offsets()
         if isinstance(index, slice):
             return [self[i] for i in range(*index.indices(len(self)))]
 
@@ -37,5 +20,37 @@ class LinesSequence(Sequence[bytes]):
 
         start, end = self._line_offsets[index]
         return self._mm[start:end]
+
+
+
+def get_line_offsets(mm: mmap.mmap) -> list[tuple[int, int]]:
+    offsets = []
+    start = 0
+    mm_len = len(mm)
+
+    while start < mm_len:
+        end = mm.find(b"\n", start)
+        if end == -1:
+            # last line without '\n'
+            offsets.append((start, mm_len))
+            break
+        offsets.append((start, end + 1))
+        start = end + 1
+    return offsets
+
+@contextmanager
+def prepare_lines_sequence(file_path: str) :
+    f = open(file_path, 'rb')
+    try:
+        if os.fstat(f.fileno()).st_size == 0:
+            # Handle empty file case
+            yield []
+        else:
+            with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
+                offsets = get_line_offsets(mm)
+                yield LinesSequence(mm, offsets)
+
+    finally:
+        f.close()
 
 
